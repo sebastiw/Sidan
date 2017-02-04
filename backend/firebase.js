d573100f53a536846f60256e1c7c8724d3f17de7
onsjo <johan.onsjo@gmail.com>
2017-02-04 15:47:15 +0100

    separated backend

---
+++ backend/firebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAKF344R4FoDaZ57f3oUxrpQBQeTVcFP3A",
+    authDomain: "sidan-b7a06.firebaseapp.com",
+    databaseURL: "https://sidan-b7a06.firebaseio.com",
+    storageBucket: "sidan-b7a06.appspot.com",
+    messagingSenderId: "938225084693"
+};
+firebase.initializeApp(config);
